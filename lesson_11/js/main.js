93d19e65292b5fd6de5a6b4c10a606a9709c4608
danil_stepanov <danil.stepanov.97@gmail.com>
2021-02-27 10:07:18 +0200

    Добавлен урок №11 слайдер

---
+++ lesson_11/js/main.js
@@ -0,0 +1,31 @@
+const slides = document.querySelectorAll('.slider__content'),
+ prev = document.querySelector('.slider-prev'),
+ next = document.querySelector('.slider-next');
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+
+
+function showSlides(n) {
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+   slides.forEach(item => item.style.display = 'none');
+   slides[slideIndex - 1].style.display = 'block';
+}
+function plusSlides(n) {
+    showSlides(slideIndex +=n);
+}
+
+prev.addEventListener('click', () => {
+    plusSlides(-1);
+})
+
+next.addEventListener('click', () => {
+    plusSlides(1);
+})
